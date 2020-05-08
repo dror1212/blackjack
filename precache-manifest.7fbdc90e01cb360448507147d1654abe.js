6216452c306e8a52bf97c65f5b8a2eb313a509fe
Dror Tal <dror.tal@ls-techs.com>
2020-05-08 17:39:02 +0300

    Updates

---
+++ precache-manifest.7fbdc90e01cb360448507147d1654abe.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3cad05b8c8494956492c395cfdf4b6",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "daf529be15e4ee245f02",
+    "url": "/blackjack/static/css/2.92aae801.chunk.css"
+  },
+  {
+    "revision": "f98c8c685d005b5bdbc5",
+    "url": "/blackjack/static/css/main.345e5324.chunk.css"
+  },
+  {
+    "revision": "daf529be15e4ee245f02",
+    "url": "/blackjack/static/js/2.72d90c08.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/blackjack/static/js/2.72d90c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f98c8c685d005b5bdbc5",
+    "url": "/blackjack/static/js/main.73c83708.chunk.js"
+  },
+  {
+    "revision": "595eef2f7276a22ba78e",
+    "url": "/blackjack/static/js/runtime-main.9b4a0421.js"
+  },
+  {
+    "revision": "2d4c18664da3f6e610f3b21c7f21e0fa",
+    "url": "/blackjack/static/media/bg.2d4c1866.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/blackjack/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/blackjack/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/blackjack/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/blackjack/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/blackjack/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/blackjack/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/blackjack/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/blackjack/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/blackjack/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/blackjack/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/blackjack/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/blackjack/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/blackjack/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/blackjack/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/blackjack/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/blackjack/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
